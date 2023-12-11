3fec67f3e4b03e8eb5c66b8250a3d3f6ac599120
syarls <charlesdavereyes18@gmail.com>
2023-12-12 03:16:34 +0800

    Forgot Password: Done

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,8 +1,12 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+{{-- <head>
+    <link rel="stylesheet" href="{{ asset('assets/css/forgotpass.css') }}">
+</head>
+
+
+
+        <div class="container">
+            <div class="card">
+        
 
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
@@ -30,5 +34,60 @@
                 </x-button>
             </div>
         </form>
-    </x-authentication-card>
+            </div></div>
+
+ --}}
+<head>
+    <link rel="stylesheet" href="{{ asset('assets/css/forgotpass.css') }}">
+</head>
+
+<x-guest-layout>
+    <div class="container">
+        <div class="card">
+            <p class="login">Forgot Password</p>
+            <x-slot name="logo">
+                <x-authentication-card-logo />
+            </x-slot>
+            <div class="circle-center">
+                <div class="circle">
+                <img class ="logo" src="{{ asset('/images/logo.png') }}" width="150px" height="150px">
+                </div>
+                </div>
+            
+            
+    
+
+    <div class="mt-4 text-sm text-gray-600 dark:text-gray-400">
+        {{ __("Don't worry if you forgot your password! Get back in within minutes by entering your email address below.") }}
+    </div>
+
+    @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+            {{ session('status') }}
+        </div>
+    @endif
+
+    <x-validation-errors class="mb-4" />
+
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+
+        <div class="block">
+            <x-label  class="mt-4" for="email" value="{{ __('Email') }}" />
+            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <x-button>
+                {{ __('Email Password Reset Link') }}
+            </x-button>
+        </div>
+    </form>
+        </div></div>
+
+
+        <br><br>
+
+    </div>
+</div>
 </x-guest-layout>
